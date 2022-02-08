8b5ea9df1c8ff32a684f6d478b0326264bd67da7
Agustin Fila <62355006+agusfila@users.noreply.github.com>
2022-02-08 17:19:47 -0300

    Add files via upload

---
+++ Fila_Agustin_Challenge/src/test/java/com/fit/Fila_Agustin_Challenge/FilaAgustinChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.Fila_Agustin_Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaAgustinChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
